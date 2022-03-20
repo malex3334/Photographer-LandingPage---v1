1cc07c61019eb5c681e3b7776416824b781a6369
Alex <alex3334@gmail.com>
2022-03-20 10:10:26 +0100

    initial

---
+++ Werka/script.js
@@ -0,0 +1,34 @@
+//mobile nav toggle //
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+/// sroll to top button
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style =
+      "opacity: 0.9; pointer-events: auto; transform: translatex(0px);";
+  } else {
+    mybutton.style.opacity = "0";
+    mybutton.style = "opacity: 0; pointer-events: none;";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
